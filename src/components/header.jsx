0baf9718bcf035abd5cce377554c356e863c38dd
Tarun Monga <tarunmonga2208@gmail.com>
2023-11-01 18:19:17 +0530

    rereer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header(){
         <div className={css.cont}>
         <img className = {css.images}src="download.jpeg"/> 
         <h1>
-        Keep It in your pants
+        Keep It
         </h1>
         </div>
         </header>
